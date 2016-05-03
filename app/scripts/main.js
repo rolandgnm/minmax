889ef8245e3f9c1f3366ee58cc748c7a4d21adf0
Roland Gabriel <rolandgnm@gmail.com>
2016-05-03 09:20:41 -0300

    Retrieving ID from view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,19 +4,29 @@
 'use strict';
 
 var CROSS_TURN = true;
+var nodes = [[],[],[]];
 
 // TODO: var hasCross= function (elem ) {
 // TODO: var resetGameField = function (positionSet);
 
 $(document).ready(function(){
   // Initialization
-
-
+  var initializeGame = function () {
+    $('.field-cell').each(function (index, elem) {
+      nodes[$(elem).parent().data('id')].push(0);
+      if ($(elem).hasClass('cross')) { $(elem).removeClass('cross'); }
+      if ($(elem).hasClass('circle')) { $(elem).removeClass('circle'); }
+    });
+  }
+  initializeGame();
 
 
   $('.field-cell').click(function(){
-    console.log(this);
     var node = $(this);
+    var parentId = node.parent().data('id');
+    var nodeId = node.data('id');
+    console.log(parentId + ' ' + nodeId);
+
     if (CROSS_TURN) {
       if (node.hasClass('cross')) { node.removeClass('cross'); }
       node.addClass('circle');
